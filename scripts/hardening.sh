be1b6d14fb59f1002f61fe8d7ca32f05a01b7745
gem-RishabhZutshi <88335034+gem-RishabhZutshi@users.noreply.github.com>
2023-02-28 22:11:10 +0530

    Update hardening.sh

--- scripts/hardening.sh
+++ scripts/hardening.sh
@@ -4,19 +4,20 @@
 # We can't directly copy the conf files to "root" owned directories via packer file provisioned
 # So we put the files in /tmp and move them by executing this shell script
 
-sudo cp /tmp/files/cis-limits.conf /etc/security/limits.d
-sudo cp /tmp/files/cis-modprobe.conf /etc/modprobe.d
-sudo cp /tmp/files/cis-sysctl.conf /etc/sysctl.d
-sudo cp /tmp/files/sshd_config /etc/ssh
-sudo cp /tmp/files/ssh_banner /etc/ssh
-sudo cp /tmp/files/00-motd-warning /etc/update-motd.d
-sudo cp /tmp/files/00-rsyslog-permissions.conf /etc/rsyslog.d
+# sudo cp /tmp/files/cis-limits.conf /etc/security/limits.d
+# sudo cp /tmp/files/cis-modprobe.conf /etc/modprobe.d
+# sudo cp /tmp/files/cis-sysctl.conf /etc/sysctl.d
+# sudo cp /tmp/files/sshd_config /etc/ssh
+# sudo cp /tmp/files/ssh_banner /etc/ssh
+# sudo cp /tmp/files/00-motd-warning /etc/update-motd.d
+# sudo cp /tmp/files/00-rsyslog-permissions.conf /etc/rsyslog.d
 
-sudo chmod 600 /etc/ssh/sshd_config
-sudo chmod 755 /etc/update-motd.d/00-motd-warning
-sudo chmod 600 /boot/grub2/grub.cfg 
+# sudo chmod 600 /etc/ssh/sshd_config
+# sudo chmod 755 /etc/update-motd.d/00-motd-warning
+# sudo chmod 600 /boot/grub2/grub.cfg 
 
-sudo find /var/log -type f -exec chmod g-wx,o-rwx {} +
-sudo chmod og-rwx /etc/cron*
+# sudo find /var/log -type f -exec chmod g-wx,o-rwx {} +
+# sudo chmod og-rwx /etc/cron*
 
-rm -rf /tmp/files
+# rm -rf /tmp/files
+sudo ansible-playbook -i /etc/ansible/hosts /tmp/files/site.yml --become
